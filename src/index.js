214018249931bc6332f5d16cc4f929a4faff6317
Hamza <hamzaoutdoors@gmail.com>
2021-08-31 20:04:36 +0100

    Add HTML markup wiht some styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import _ from 'lodash';
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import './style.css';
+//import load from './modules/load';
+
+//window.addEventListener('DOMContentLoaded', load);

9703793fea6b1006cbe65cbe551266c85561c4f0
Hamza <hamzaoutdoors@gmail.com>
2021-08-31 20:44:30 +0100

    Add functionality to add player's score dynamicaly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
 import _ from 'lodash';
 import './style.css';
-//import load from './modules/load';
 
-//window.addEventListener('DOMContentLoaded', load);
+const players = [
+  {
+    name: 'Name',
+    score: 100,
+  },
+  {
+    name: 'Hamza',
+    score: 40,
+  },
+  {
+    name: 'Larry',
+    score: 12,
+  },
+  {
+    name: 'Ewa',
+    score: 70,
+  },
+  {
+    name: 'Jacob',
+    score: 55,
+  },
+  {
+    name: 'Carlos',
+    score: 60,
+  },
+];
+
+const render = () => {
+  const table = document.getElementById('score-table');
+  players.forEach((player, index) => {
+    const { name, score } = player;
+    table.innerHTML += `<tr id="user-${index}">
+    <td id="name-${index}">${name}</td>
+    <td id="score-${index}">${score}</td>
+    </tr>`;
+  });
+};
+
+render();

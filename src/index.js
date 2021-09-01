0e736d529227b1603956890476145a2539806a67
Hamza <hamzaoutdoors@gmail.com>
2021-09-01 23:45:32 +0100

    Add events to buttons and work with API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
-import _ from 'lodash';
+/* eslint-disable no-unused-vars */
+import 'lodash';
 import './style.css';
-// import render from './modules/renderScores';
-import createNewGame from './modules/newGame.js';
+import render from './modules/renderScores.js';
+import { postApi, getApi } from './modules/apiInteracting.js';
+
+const urlGame = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/VhnmOHGBsQdsWSdfuXHy/scores/';
+const form = document.getElementById('form');
+const refresh = document.getElementById('refresh');
+const table = document.getElementById('score-table');
+
+refresh.addEventListener('click', () => {
+  getApi(urlGame)
+    .then((response) => {
+      table.innerHTML = '';
+      render(response.result);
+    });
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  postApi()
+    .then(() => {
+      document.forms[0].reset();
+    });
+});
+
+getApi(urlGame)
+  .then((res) => {
+    render(res.result);
+  });

93fbca061d1049eebc079d213928415c72e0cd09
Hamza <hamzaoutdoors@gmail.com>
2021-09-01 21:12:59 +0100

    Add modules to make the projects looks modular

--- src/index.js
+++ src/index.js
@@ -1,43 +1,4 @@
-/* eslint-disable no-unused-vars */
 import _ from 'lodash';
 import './style.css';
-
-const players = [
-  {
-    name: 'Name',
-    score: 100,
-  },
-  {
-    name: 'Hamza',
-    score: 40,
-  },
-  {
-    name: 'Larry',
-    score: 12,
-  },
-  {
-    name: 'Ewa',
-    score: 70,
-  },
-  {
-    name: 'Jacob',
-    score: 55,
-  },
-  {
-    name: 'Carlos',
-    score: 60,
-  },
-];
-
-const render = () => {
-  const table = document.getElementById('score-table');
-  players.forEach((player, index) => {
-    const { name, score } = player;
-    table.innerHTML += `<tr id="user-${index}">
-    <td id="name-${index}">${name}</td>
-    <td id="score-${index}">${score}</td>
-    </tr>`;
-  });
-};
-
-render();
+// import render from './modules/renderScores';
+import createNewGame from './modules/newGame.js';
